373acdb60f036f23d843a93d6b165b09f5a6794e
akolosov <jora364@gmail.com>
2020-01-05 13:31:13 +0300

    build

---
+++ lib/index.d.ts
@@ -0,0 +1,6 @@
+declare const _default: {
+    rules: {
+        "variables-case": import("@typescript-eslint/experimental-utils/dist/ts-eslint/Rule").RuleModule<"0" | "1" | "2", [], import("@typescript-eslint/experimental-utils/dist/ts-eslint/Rule").RuleListener>;
+    };
+};
+export = _default;
